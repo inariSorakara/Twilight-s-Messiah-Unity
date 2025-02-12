6c7d803e93905af5fafeb77ebcd0d49d61bbc2f5
Inari Sorakara <maximiliano.torres.nr@gmail.com>
2025-02-12 13:39:13 -0300

    Create RegularRoom.cs

---
+++ RegularRoom.cs
@@ -0,0 +1,16 @@
+using UnityEngine; // Imports Unity's core functionality.
+
+public class RegularRoom : MonoBehaviour //Defines the class
+{
+    // Called once when the script is loaded. Akin to ready()
+    void Start()
+    {
+     Debug.Log("Room is ready");
+    }
+
+    // Update is called once per frame. Akin to Update(delta)
+    void Update()
+    {
+        
+    }
+}
