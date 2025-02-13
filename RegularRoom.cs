552c3454db5fba7e61d2ff80130026ae46b0f619
Inari Sorakara <maximiliano.torres.nr@gmail.com>
2025-02-13 13:18:45 -0300

    Update RegularRoom.cs

--- RegularRoom.cs
+++ RegularRoom.cs
@@ -2,10 +2,16 @@ using UnityEngine; // Imports Unity's core functionality.
 
 public class RegularRoom : MonoBehaviour //Defines the class
 {
+    // Variables accessible anywhere inside this RegularRoom instance.
+    public string room_coordinate;
+    public string room_event_type;
+
     // Called once when the script is loaded. Akin to ready()
     void Start()
     {
-     Debug.Log("Room is ready");
+     room_coordinate = "A1";
+     room_event_type = "Quartz";
+     Debug.Log("Room is ready with coordinate: " + room_coordinate + " and event type: " + room_event_type);
     }
 
     // Update is called once per frame. Akin to Update(delta)
